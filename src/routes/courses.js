5fee90f0d6320d257e599df7fd39f0798b35cd99
TuanLA <leanhtuan@gmail.com>
2021-05-24 17:17:07 +0700

    delete product

--- src/routes/courses.js
+++ src/routes/courses.js
@@ -7,6 +7,8 @@ router.post('/store', sourceController.store);
 
 router.put('/:id', sourceController.update);
 
+router.delete('/:id', sourceController.destroy);
+
 router.get('/create', sourceController.create);
 router.get('/:id/edit', sourceController.edit);
 router.get('/:slug', sourceController.show);
